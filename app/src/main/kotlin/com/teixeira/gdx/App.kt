bf9cd6977016efd0496de6faa7b1e72143446bf7
teixeira0x <secretedit123e@gmail.com>
2024-08-27 07:26:38 -0300

    add possibility to create project without command line

--- app/src/main/kotlin/com/teixeira/gdx/App.kt
+++ app/src/main/kotlin/com/teixeira/gdx/App.kt
@@ -16,11 +16,23 @@ import org.apache.commons.cli.CommandLine
 import org.apache.commons.cli.DefaultParser
 
 fun main(args: Array<String>) {
-  exitProcess(App().run(args))
+  val app = App()
+  exitProcess(
+    if (args.isNotEmpty()) {
+      app.run(args)
+    } else {
+      app.runApp()
+    }
+  )
 }
 
 class App {
 
+  companion object {
+    val REGEX_JAVA_OR_KOTLIN = Regex("^(java|kotlin)$")
+  }
+
+  // Run command
   fun run(args: Array<String>): Int {
     val command: CommandLine? =
       try {
@@ -41,12 +53,62 @@ class App {
     val minSdk = command.getOptionValue(OPT_MIN_SDK, DEFAULT_MIN_SDK).trim()
     val targetSdk = command.getOptionValue(OPT_TARGET_SDK, DEFAULT_TARGET_SDK).trim()
 
-    if (!language.matches(Regex("^(java|kotlin)$"))) {
+    if (!language.matches(REGEX_JAVA_OR_KOTLIN)) {
       print("Choose between kotlin or java language.")
       return 1
     }
 
     val writer = ProjectWriter(language, name, packageName, minSdk, targetSdk)
+    writer.write { message -> println(message) }
+
+    return 0
+  }
+
+  // Run application
+  fun runApp(): Int {
+    println("Welcome to the LibGDX project builder for AndroidIDE!")
+    println("Let's start creating")
+    println()
+
+    print("Enter the project name:")
+    val name = readLine()
+    if (name?.isEmpty() ?: true) {
+      println("The name cannot be empty!")
+      return 1
+    }
+
+    print("Enter the app package:")
+    val packageName = readLine()
+    if (packageName?.isEmpty() ?: true) {
+      println("The app package cannot be empty!")
+      return 1
+    }
+
+    print("Enter the project language (Default $DEFAULT_LANGUAGE):")
+    var language = readLine()
+    if (language?.isEmpty() ?: true) {
+      language = DEFAULT_LANGUAGE
+    }
+
+    if (!language!!.matches(REGEX_JAVA_OR_KOTLIN)) {
+      print("Choose between kotlin or java language.")
+      return 1
+    }
+
+    print("Enter the project minSdk (Default $DEFAULT_MIN_SDK):")
+    var minSdk = readLine()
+    if (minSdk?.isEmpty() ?: true) {
+      minSdk = DEFAULT_MIN_SDK
+    }
+
+    print("Enter the project targetSdk (Default $DEFAULT_TARGET_SDK):")
+    var targetSdk = readLine()
+    if (targetSdk?.isEmpty() ?: true) {
+      targetSdk = DEFAULT_TARGET_SDK
+    }
+    println()
+
+    val writer = ProjectWriter(language!!, name!!, packageName!!, minSdk!!, targetSdk!!)
     writer.write { message -> println(message) }
 
     return 0
